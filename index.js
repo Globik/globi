843b1c94590d0be6c60b44697639bd894789d2b9
Globik <petrov.ag412@yandex.ru>
2014-12-26 22:15:39 +0400

    ft

--- index.js
+++ index.js
@@ -2,23 +2,25 @@
 var express = require('express');
 var path=require('path');
 
-var favicon=require('serve-favicon');
-var logger=require('morgan');
-var cooki=require('cookie-parser');
-var bodyparser=require('body-parser');
+//var favicon=require('serve-favicon');
+//var logger=require('morgan');
+//var cooki=require('cookie-parser');
+//var bodyparser=require('body-parser');
 
 var app = express();
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(logger('dev'));
-app.use(bodyparser.json());
+//app.use(logger('dev'));
+//app.use(bodyparser.json());
+/***
 app.use(bodyparser.urlencoded());
 app.use(cooki());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(__dirname + '/public'));
+***/
 //app.use(bodyparser());
 //app.use(methodover());
  //app.use(express.router());
@@ -29,7 +31,7 @@ app.get('/', function(req, res) {
 });
 ***/
 /***
-var router = express.Router();
+//var router = express.Router();
 
 ***/
 //var db=require('mongoskin').db(process.env.MONGOHQ_URL,{w:1});
@@ -42,6 +44,7 @@ var drinks=[
 {name:'Alik',drunk:5},
 {name:'Dima',drunk:10}
 ];
+
   res.render('index', { drinks:drinks, title: 'Express' });
 });
 /***
