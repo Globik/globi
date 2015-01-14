e586375b170c525ab780fe5f013135bec3bf85ac
Globik <petrov.ag412@yandex.ru>
2015-01-14 20:46:22 +0400

    l

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var flash = require('connect-flash')
+var express = require('express');
+var path=require('path');
+var passport=require('passport');
+var cookieParser=require('cookie-parser');
+var bodyparser=require('body-parser');
+var met=require('method-override');
+
 var express = require('express');
 var app = express();
 
