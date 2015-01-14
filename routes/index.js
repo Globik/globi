6a5a9b3bc5e66ffe4b354be5b120cb337ff9047a
Globik <petrov.ag412@yandex.ru>
2015-01-14 20:43:42 +0400

    l

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,13 @@
 var express = require('express');
-var passport=require('passport');
-var router = express.Router();
+var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 
-router.get('/', function(req, res) {
-var db=req.db;
-var drinks=[
-{name:'Bloody Mary',drunk:3},
-{name:'Alik',drunk:5},
-{name:'Dima',drunk:10}
-];
-
-db.collection('tasks').find().toArray(function(err,result){
-if(err)throw err;
-
-  res.render('index', { drinks:drinks, title: 'Express',user:req.user,resul:result});
-});});
-
-router.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
+app.get('/', function(request, response) {
+ 
+  response.send("  Hello world and Globi!!!");
 });
 
-
-router.get('/login', function(req, res){
-  res.render('login', { user: req.user, message: req.flash('error') });
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
-
-router.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
-  function(req, res) {
-    res.redirect('/');
-  });
-
-router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
-}
-
-
-/* GET home page. */
-/***
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-***/
-
-module.exports = router;
