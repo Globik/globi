10aba11632492aa942c9965d10b4ef0060e185df
Globik <petrov.ag412@yandex.ru>
2015-12-13 16:25:09 +0300

    sd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 //node index
-var develop="developnt";
+var develop="development";
 var http=require('http');
 var express=require('express');
 var path=require('path');
@@ -11,7 +11,7 @@ var passport=require('passport');
 var cookieParser=require('cookie-parser');
 var methodOverride=require('method-override');
 var session=require('express-session');
-var MongoStore=require('connect-mongo')(session);
+//var MongoStore=require('connect-mongo')(session);
 var bodyParser=require('body-parser');
 var multer=require('multer');
 var errorHandler=require('errorhandler');
@@ -21,6 +21,7 @@ var db={};
 //var url="mongodb://127.0.0.1:27017/todo";
 var url=process.env.MONGOHQ_URL
 //var url=process.env.MONGOHQ_URL_TEST;
+/*
 var MongoClient=require('mongodb').MongoClient;
 var ObjectID=require('mongodb').ObjectID;
 MongoClient.connect(url,(er,dob)=>{
@@ -28,6 +29,7 @@ MongoClient.connect(url,(er,dob)=>{
 	else{console.log('db connected');db.b=dob;}
 require('./config/passport')(db.b,passport);
 });
+*/
 //var crypto=require('crypto');
 
 var app=express();
@@ -40,27 +42,29 @@ app.use(favicon(__dirname+'/public/w4.png'));
 //var user={_id:1,username:"Bob",password:"secret"};
 
 if(develop==app.get('env')){app.use(logger('dev'));}
-app.use(methodOverride());
-app.use(cookieParser('secret','mysecret'));
+//app.use(methodOverride());
+//app.use(cookieParser('secret','mysecret'));
 app.use(session({/*cookie:{httpOnly:true,secure:false,maxAge:60*60*4000},*/
 resave:false,saveUninitialized:true,secret:'bred'/*,store:new MongoStore({url})*/}));
-app.use(flash());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(multer({dest:'./uploads'}).single('singleInputFileName'));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname,'public')));
-
+//app.use(flash());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended:true}));
+//app.use(multer({dest:'./uploads'}).single('singleInputFileName'));
+//app.use(passport.initialize());
+//app.use(passport.session());
+//app.use(express.static(path.join(__dirname,'public')));
+/*
 app.use((req,res,next)=>{
 let redstr=fs.createReadStream('./advert-config.json');
 redstr.on('error',(er)=>{console.log('err readstr :',er)});
 redstr.on('data',(d)=>{
-//console.log('d json :',JSON.parse(d.toString()).advertmodule);
+
 req.advdata=JSON.parse(d.toString());
 req.db=db.b;
 next();
 })});
+*/
+/*
 const limit=10;
 function pagination(req,res,next){
 	var qu=parseInt(req.params.page) || 1;
@@ -90,7 +94,7 @@ if(num>limit) {res.locals.prev=true;}
 next();
 	});
 }
-
+*/
 //node --harmony --harmony_destructuring --harmony_rest_parameters --harmony_modules index
 //node --harmony index
 /*
@@ -106,11 +110,12 @@ if(event=='change'){if(!flipbit){console.log("DOING this Path :",pat);}else{
 var temp=path.join(viewsDir,pat);reload(temp);}flipdas();}});
 */
 var router=express.Router();
-var haupt_page=require('./views/haupt_page.js');
-var login=require('./views/login');
+//var haupt_page=require('./views/haupt_page.js');
+//var login=require('./views/login');
 //supervisor --harmony --harmony_destructuring -w views index 
 //supervisor --harmony --harmony_destructuring views index
 //supervisor --force-watch views index
+/*
 router.get('/',(req,res)=>{
 	var adv=req.advdata;
 	var bib=haupt_page.haupt_page({showmodule:adv.advertmodule,buser:req.user});
@@ -132,8 +137,9 @@ router.get('/articles',pagination,(req,res)=>{
 	});
 router.get('/labs',(req,res)=>{
 	res.send("labs")
-});
+}); */
 //node index
+/*
 var articles_page_page=require('./views/articles_page_page');
  router.param('page',(req,res,next,page)=>{
 	if(isNumb(page)==false){res.sendStatus(404);}else if(page==0){res.sendStatus(404)}else{next();}
@@ -154,7 +160,10 @@ router.get('/logout',(req,res)=>{req.logout();res.redirect('/')});
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login');
-}
+}*/
+router.get('/',(req,res)=>{
+	res.send("Hallo world");
+})
 app.use('/',router);
 if(develop==app.get('env')){app.use(errorHandler());}
 //node --harmony app
